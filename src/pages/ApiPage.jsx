23c8c1ac00ed99b318bb8459455f6e5d2c55e5ce
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-11-04 21:29:17 +0400

    add data on the apiPage from api by axios

--- src/pages/ApiPage.jsx
+++ src/pages/ApiPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect} from 'react';
+import axios from 'axios';
 
 function ApiPage() {
+  const [dateItem,setDateItem]=useState([]);
+
+  useEffect(()=>{
+    const requestUser=async()=>{
+      const response=await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const data =response.data;
+      setDateItem(data);
+    };
+
+    requestUser();
+  },[]);
   return (
-    <div>ApiPage</div>
+    <>
+    <div>{dateItem.map((user)=>(
+      <div>
+        <div key={user.id}>{user.title}</div>
+      <div key={user.id}>{user.body}</div>
+      </div>
+
+    ))}</div>
+    </>
   )
 }
 
